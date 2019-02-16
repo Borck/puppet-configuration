d5e0c97aac28b47f4150793a55afe1ff481547a5
Borck <borck@hotmail.de>
2019-02-16 01:10:29 +0100

    [BUGFIX] fix removing of file context menu items 'Scan with Defender' and 'Add to library' [UPDATE] workaround some package that are not updatable with setting it to 'present' (instead of 'latest') [ADDED] icons for many archive type like 7z and rar [UPDATE] removed '3D objects' from 'This PC' [UPDATE] set File Explorer start directory to 'This PC'

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,12 +1,46 @@
+define reg_ensure_file_ext(String $display_name, String $icon) {
+  registry_key   { "HKCR\\${name}file": ensure => present, }
+  registry_value { "HKCR\\${name}file\\": type => string, data => $display_name }
+  registry_key   { "HKCR\\${name}file\\defaulticon": ensure => present, }
+  registry_value { "HKCR\\${name}file\\defaulticon\\": type => string, data => $icon }
+  registry_key   { "HKCR\\${name}file\\shell": ensure => present, }
+  registry_key   { "HKCR\\.${name}": ensure => present, }
+  registry_value { "HKCR\\.${name}\\": type => string, data => "${name}file" }
+}
+
+define reg_ensure_file_ext_value(String $value) {
+  registry_value { "HKCR\\.${name}": type => string, data => $value }
+}
+
+define reg_ensure_archive_ext(String $icondirectory) {
+  reg_ensure_file_ext { $name: display_name => "${name} Archive", icon => "${icondirectory}\\${name}.ico" }
+  #registry_key   { "HKCR\\${name}file": ensure => present, }
+  #registry_value { "HKCR\\${name}file\\": type => string, data => "${name} Archive" }
+  #registry_key   { "HKCR\\${name}file\\defaulticon": ensure => present, }
+  #registry_value { "HKCR\\${name}file\\defaulticon\\": type => string, data => "${icondirectory}\\${name}.ico" }
+  registry_key   { "HKCR\\${name}file\\shell\\open\\command": ensure => present, }
+  registry_value { "HKCR\\${name}file\\shell\\": type => string, data => 'open' }
+  registry_value { "HKCR\\${name}file\\shell\\open\\command\\": type => string, data => '"C:\\Program Files\\7-Zip\\7zFM.exe" "%1"' }
+  registry_value { "HKCR\\${name}file\\shell\\open\\Icon": type => string, data => '"C:\\Program Files\\7-Zip\\7zFM.exe"' }
+  #registry_key   { "HKCR\\.${name}": ensure => present, }
+  #registry_value { "HKCR\\.${name}\\": type => string, data => "${name}file" }
+  registry_value { "HKCR\\.${name}\\PerceivedType": type => string, data => 'compressed' }
+}
+
 node default {
   # New modules and packages can be found here
   # https://forge.puppet.com/
   # https://chocolatey.org/
 
+  #include stdlib
 
   if $::kernel == 'windows' {
-    $isMyPc = 'Borck' in $hostname
 
+    $username = split($identity['user'],'\\\\')[1]
+    $user_sid = $user_sids[$username]
+
+    $is_my_pc = 'borck' in downcase($hostname)
+    $is_my_user = 'borck' in downcase($username)
     ###########################################################################
     ########## Package repositories ###########################################
     ###########################################################################
@@ -17,41 +51,13 @@ node default {
 
 
     ###########################################################################
-    ########## Media tools ####################################################
-    ###########################################################################
-
-    package { 'vlc': ensure => latest, }
-    registry_key { 'HKCR\Directory\shell\AddToPlaylistVLC': ensure => present, }
-    registry_value { 'HKCR\Directory\shell\AddToPlaylistVLC\LegacyDisable': ensure => present, type => string, }
-    registry_key { 'HKCR\Directory\shell\PlayWithVLC': ensure => present, }
-    registry_value { 'HKCR\Directory\shell\PlayWithVLC\LegacyDisable': ensure => present, type => string, }
-
-    package { 'inkscape': ensure => latest, }
-
-    package { 'caesium.install': ensure => present, }
-    package { 'handbrake': ensure => present, }
-    package { 'FileOptimizer': ensure => present, }
-
-    package { 'audacity': ensure => present, }
-    package { 'audacity-lame': ensure => present, }
-
-    package { 'Calibre ': ensure => present, } # convert * to  ebook
-
-    if $isMyPc {
-      #package { 'itunes': ensure => latest, }  #used MS Store version
-      package { 'mp3tag': ensure => present, }
-
-      # package { 'vcredist2008': ensure => present, } # install issue
-      package { 'picard': ensure => present, } # MusicBrainz Picard, music tags online grabber, requires 'vcredist2008'
-    }
-
-    ###########################################################################
     ########## Office #########################################################
     ###########################################################################
 
     package { 'firefox': ensure => present, } #firefox have a very silent update mechanism
+
     package { 'EdgeDeflector ': ensure => latest, } #redirects URIs to the default browser (caution: menu popup)
-    
+
 
     # msoffice { 'office 2016':
     #   version     => '2016',
@@ -77,16 +83,15 @@ node default {
     # rainmeter is unofficial and not a silent installer
     # package { 'rainmeter': ensure => latest, }
 
-    if $isMyPc {
+    if $is_my_pc {
       # package { 'cloudstation ': ensure => present, } # Synology Cloud Station Drive, synology drive used instead
-      
+
       package { 'ghostscript': ensure => present, }
       package { 'miktex': ensure => present, }
       package { 'texstudio': ensure => present, }
       package { 'jabref': ensure => present, }
-
+      package { 'yed': ensure => present, }
       # package { 'dropbox': ensure => present, } # not yet installed
-
     } else {
       package { 'googlechrome': ensure => present, }
       package { 'adobereader': ensure => present, }
@@ -95,19 +100,57 @@ node default {
 
 
     ###########################################################################
+    ########## Media tools ####################################################
+    ###########################################################################
+
+    package { 'vlc': ensure => latest, }
+    registry_key { 'HKCR\Directory\shell\AddToPlaylistVLC': ensure => present, }
+    registry_value { 'HKCR\Directory\shell\AddToPlaylistVLC\LegacyDisable': ensure => present, type => string, }
+    registry_key { 'HKCR\Directory\shell\PlayWithVLC': ensure => present, }
+    registry_value { 'HKCR\Directory\shell\PlayWithVLC\LegacyDisable': ensure => present, type => string, }
+
+    package { 'inkscape': ensure => present, }
+
+    package { 'caesium.install': ensure => present, }
+    file { 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Caesium\Caesium - Image Converter.exe':
+      ensure => 'link',
+      target => 'C:\\Program Files (x86)\\Caesium\\Caesium.exe',
+    }
+    file { 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Caesium\Caesium.lnk':
+      ensure => absent,
+    }
+
+    package { 'handbrake': ensure => present, }
+    package { 'FileOptimizer': ensure => present, }
+
+    package { 'audacity': ensure => present, }
+    package { 'audacity-lame': ensure => present, }
+
+    package { 'Calibre ': ensure => present, } # convert * to  ebook
+
+    if $is_my_pc {
+      #package { 'itunes': ensure => latest, }  #used MS Store version
+      package { 'mp3tag': ensure => present, }
+
+      # package { 'vcredist2008': ensure => present, } # install issue
+      package { 'picard': ensure => present, } # MusicBrainz Picard, music tags online grabber, requires 'vcredist2008'
+    }
+
+    ###########################################################################
     ########## Development ####################################################
     ###########################################################################
 
-    package { 'jdk8': ensure => latest, }
+    package { 'jdk8': ensure => present, }
 
-    if $isMyPc {
+    if $is_my_pc {
 
-      package { 'eclipse': ensure => latest, install_options => ['--params', '"/Multi-User"'], }
+      package { 'eclipse': ensure => present, install_options => ['--params', '"/Multi-User"'], }
 
       package { 'make': ensure => present, }
-      package { 'cmake': ensure => latest, install_options => ['--installargs', '"DESKTOP_SHORTCUT_REQUESTED=0"'], }
+      #package { 'cmake': ensure => latest, install_options => ["--installargs", "'DESKTOP_SHORTCUT_REQUESTED=0'", 
+      #  "'ADD_CMAKE_TO_PATH=System'", "'ALLUSERS=1'"], }
 
-      package { 'virtualbox': ensure => latest, install_options => ['/NoDesktopShortcut', '/NoQuickLaunch'], }
+      # package { 'virtualbox': ensure => latest, install_options => ['--params', '/NoDesktopShortcut', '/NoQuickLaunch'], }
       # virtualbox.extensionpack is included in package virtualbox
       # package { 'virtualbox.extensionpack': ensure => latest, }
 
@@ -131,12 +174,13 @@ node default {
       registry_value { 'HKCR\Directory\Background\shell\git_shell\LegacyDisable': ensure => present, type => string, }
 
       # installing tortoisegit can fail if non-package-version is installed
-      package { 'tortoisegit': ensure => latest, }
+      package { 'tortoisegit': ensure => present, }
 
-      package { 'tortoisesvn': ensure => latest, }
+      package { 'tortoisesvn': ensure => present, }
 
-      package { 'sourcetree': ensure => latest, }
+      package { 'sourcetree': ensure => present, }
 
+      # package { 'python3': ensure => '3.6.0', install_options => ['--params', '/InstallDir', '"c:\\program', 'files\\Python\\Python36"']}
     }
 
     ###########################################################################
@@ -144,7 +188,7 @@ node default {
     ###########################################################################
     # code for Visual Studio (not Visual Studio Code is at the and of this script)
 
-    package { 'visualstudiocode': ensure => latest, install_options => ['--params', "'/NoDesktopIcon", "/NoQuicklaunchIcon'"],}
+    package { 'visualstudiocode': ensure => present, install_options => ['--params', "'/NoDesktopIcon", "/NoQuicklaunchIcon'"],}
 
     #https://forge.puppet.com/tragiccode/vscode
     # class { 'vscode':
@@ -172,7 +216,8 @@ node default {
 
     file { 'C:\ProgramData\NotepadReplacer\notepad.exe':
       ensure => 'link',
-      target => 'C:\Program Files\Microsoft VS Code\code.exe',
+      target => 'C:\\Program Files\\Microsoft VS Code\\code.exe',
+      #target => "C:\\Users\\${username}\\AppData\\Local\\Programs\\Microsoft VS Code\\code.exe",
     }
 
     package { 'notepadreplacer':
@@ -188,7 +233,7 @@ node default {
     ########## visual studio + unity ##########################################
     ###########################################################################
 
-    if $isMyPc {
+    if $is_my_pc {
       package { 'visualstudio2017enterprise': ensure => present, }
 
       package { 'visualstudio2017-workload-azure': ensure => present, }
@@ -201,18 +246,18 @@ node default {
       package { 'visualstudio2017-workload-vctools': ensure => present, }
       package { 'visualstudio2017-workload-visualstudioextension': ensure => present, }
 
-      package { 'visualsvn': ensure => latest, }
+      package { 'visualsvn': ensure => present, }
 
       # jetbrains
-      package { 'resharper': ensure => latest, }
-      package { 'dotpeek': ensure => latest, } # decompiler
-      package { 'dotcover': ensure => latest, } # unit test runner and code coverage
-      package { 'dottrace': ensure => latest, } # performance profiler
-      package { 'dotmemory': ensure => latest, } # memory profiler
+      package { 'resharper': ensure => present, }
+      package { 'dotpeek': ensure => present, } # decompiler
+      package { 'dotcover': ensure => present, } # unit test runner and code coverage
+      package { 'dottrace': ensure => present, } # performance profiler
+      package { 'dotmemory': ensure => present, } # memory profiler
 
       # package { 'unity': ensure => present, } not really required
       # Game development with Unity workload for Visual Studio 2017
-      # package { 'visualstudio2017-workload-managedgame': ensure => latest, }
+      # package { 'visualstudio2017-workload-managedgame': ensure => present, }
 
       package { 'arduino': ensure => present, }
     }
@@ -222,8 +267,8 @@ node default {
     ###########################################################################
 
     package { 'sdio': ensure => present, } # Snappy Driver Installer Origin (open source)
-    package { 'winaero-tweaker': ensure => latest, } # Snappy Driver Installer Origin (open source)
-    
+    package { 'winaero-tweaker': ensure => present, }
+
 
 
     # https://chocolatey.org/packages/desktopicons-winconfig
@@ -235,7 +280,7 @@ node default {
     package { 'curl': ensure => present, }
     package { 'wget': ensure => present, }
 
-    if $isMyPc {
+    if $is_my_pc {
       package { 'putty': ensure => latest, }
       package { 'winscp': ensure => latest, }
       package { 'wireshark': ensure => latest, }
@@ -245,7 +290,7 @@ node default {
       package { 'windirstat': ensure => present, }
       package { 'junction-link-magic': ensure => present, }
 
-      package { 'win32diskimager': ensure => present, }
+      #package { 'win32diskimager': ensure => present, }
       package { 'etcher': ensure => present, } # image to usb drive or sd card
       package { 'rufus': ensure => present, } # format/create bootable USB flash drives
 
@@ -304,6 +349,43 @@ node default {
 
 
     ###########################################################################
+    ########## File types #####################################################
+    ###########################################################################
+    $icons = 'C:\\Windows\\Icons.puppet'
+    file { $icons:
+      ensure             => present,
+      source             => 'puppet:///modules/icons',
+      source_permissions => ignore,
+      recurse            => true,
+      purge              => true,
+    }
+
+
+
+    ## archives ##
+
+    $icons_archives = "${icons}\\7_zip_filetype_theme___windows_10_by_masamunecyrus-d93yxyk"
+
+    reg_ensure_archive_ext {
+        '001' : icondirectory => $icons_archives;
+        '7z' : icondirectory => $icons_archives;
+        'bz2' : icondirectory => $icons_archives;
+        'gz' : icondirectory => $icons_archives;
+        'rar' : icondirectory => $icons_archives;
+        'tar' : icondirectory => $icons_archives;
+    }
+
+
+
+    ## graphics ##
+
+    package { 'svg-explorer-extension': ensure => present, }
+    reg_ensure_file_ext {       'svg' : display_name => 'Scalable Vector Graphics', icon => "${icons}\\svgfile.ico" }
+    reg_ensure_file_ext_value { 'svg\\Content Type'  : value => 'image/svg+xml' }
+    reg_ensure_file_ext_value { 'svg\\PerceivedType' : value => 'image' }
+
+
+    ###########################################################################
     ########## File explorer tweaks ###########################################
     ###########################################################################
 
@@ -313,7 +395,7 @@ node default {
     # Hide_Message_-_“Es_konnten_nicht_alle_Netzlaufwerke_wiederhergestellt_werden”
     registry_value { 'HKLM\SYSTEM\CurrentControlSet\Control\NetworkProvider\RestoreConnection': ensure => present, type => dword, data => '0x00000000' }
 
-    if $isMyPc {
+    if $is_my_pc {
       # how to hide element in 'This PC': http://www.thewindowsclub.com/remove-the-folders-from-this-pc-windows-10
       # hide 'Documents' in 'This PC'
       registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{f42ee2d3-909f-4907-8871-4c22fc0bf756}\PropertyBag\ThisPCPolicy': ensure => present, type => string, data => 'Hide' }
@@ -324,7 +406,7 @@ node default {
       registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{35286a68-3c57-41a1-bbb1-0eae73d76c95}\PropertyBag\ThisPCPolicy': ensure => present, type => string, data => 'Hide' }
 
       #enable checkboxes, see: https://www.tenforums.com/attachments/tutorials/35188d1441136772-turn-off-select-items-using-check-boxes-windows-10-a-select_items_with_check_boxes.png
-      # registry_value { 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\AutoCheckSelect': ensure => present, type => dword, data => 0x00000001 }
+      registry_value { "HKU\\${user_sid}\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\AutoCheckSelect": ensure => present, type => dword, data => 0x00000001 }
 
       # add quick merge to context menu of reg-files
       registry_key   { 'HKCR\regfile\shell\quickmerge\command': ensure => present, }
@@ -340,16 +422,16 @@ node default {
       registry_value { 'HKCR\DesktopBackground\Shell\Restart Explorer\command\\': ensure => present, type => string, data => 'TSKILL EXPLORER' }
 
       # remove 'Add to library' from context menu
-      registry_key   { 'HKCR\HKEY_CLASSES_ROOT\Folder\ShellEx\ContextMenuHandlers\Library Location': ensure => absent, }
+      registry_key   { 'HKCR\Folder\ShellEx\ContextMenuHandlers\Library Location': ensure => absent, }
       # backup:
-      # registry_value { 'HKCR\HKEY_CLASSES_ROOT\Folder\ShellEx\ContextMenuHandlers\Library Location\\': ensure => present, type => string, data => '{3dad6c5d-2167-4cae-9914-f99e41c12cfa}' }
+      # registry_value { 'HKCR\Folder\ShellEx\ContextMenuHandlers\Library Location\\': ensure => present, type => string, data => '{3dad6c5d-2167-4cae-9914-f99e41c12cfa}' }
 
       # remove 'Scan with Windows Defender' from context menu
-      registry_key   { 'HKCR\HKEY_CLASSES_ROOT\*\ShellEx\ContextMenuHandlers\EPP': ensure => absent, }
-      registry_key   { 'HKCR\HKEY_CLASSES_ROOT\Directory\ShellEx\ContextMenuHandlers\EPP': ensure => absent, }
+      registry_key   { 'HKCR\*\shellex\ContextMenuHandlers\EPP': ensure => absent, }
+      registry_key   { 'HKCR\Directory\shellex\ContextMenuHandlers\EPP': ensure => absent, }
       # backup:
-      # registry_value { 'HKCR\HKEY_CLASSES_ROOT\*\ShellEx\ContextMenuHandlers\EPP\\': ensure => present, type => string, data => '{09A47860-11B0-4DA5-AFA5-26D86198A780}' }
-      # registry_value { 'HKCR\HKEY_CLASSES_ROOT\Directory\ShellEx\ContextMenuHandlers\EPP\\': ensure => present, type => string, data => '{09A47860-11B0-4DA5-AFA5-26D86198A780}' }
+      # registry_value { 'HKCR\*\ShellEx\ContextMenuHandlers\EPP\\': ensure => present, type => string, data => '{09A47860-11B0-4DA5-AFA5-26D86198A780}' }
+      # registry_value { 'HKCR\Directory\ShellEx\ContextMenuHandlers\EPP\\': ensure => present, type => string, data => '{09A47860-11B0-4DA5-AFA5-26D86198A780}' }
 
 
       # Disable AutoPlay for removable media drives for CurrentUser
@@ -358,13 +440,22 @@ node default {
       # Remove 'Shortcut' from new links
       # registry_value { 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\link': ensure => present, type => binary, data => '00 00 00 00' }
       # backup: data => '1e 00 00 00'
+
+      # Remove '3D objects' from This PC
+      registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}': ensure => absent  }
+      registry_value { 'HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}': ensure => absent  }
+    }
+
+    if $is_my_user {
+      # Windows Explorer launch to ThisPC
+      registry_value { "HKU\\${user_sid}\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo": ensure => present, type => dword, data => 0x00000001,  }
     }
 
 
     ###########################################################################
     ########## Gaming #########################################################
     ###########################################################################
-    package { 'origin': ensure => latest, }
-    package { 'steam': ensure => latest, }
+    #package { 'origin': ensure => latest, }
+    package { 'steam': ensure => present, }
   }
 }
