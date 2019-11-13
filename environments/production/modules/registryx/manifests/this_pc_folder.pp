7f3deac4de80849f323baf8a85550c2e704ebbcf
Borck <borck@hotmail.de>
2019-11-13 22:31:52 +0100

    [BUGFIX] fix show/hide of Music folder in This PC not working

--- environments/production/modules/registryx/manifests/this_pc_folder.pp
+++ environments/production/modules/registryx/manifests/this_pc_folder.pp
@@ -12,7 +12,7 @@ define registryx::this_pc_folder (
     'desktop'   => '{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}',
     'documents' => '{f42ee2d3-909f-4907-8871-4c22fc0bf756}',
     'downloads' => '{374DE290-123F-4565-9164-39C4925E467B}',
-    'music'     => '{1CF1260C-4DD0-4ebb-811F-33C572699FDE}',
+    'music'     => '{a0c69a99-21c8-4671-8703-7934162fcf1d}',
     'pictures'  => '{0ddd015d-b06c-45d5-8c4c-f59713854639}',
     #'recycling bin' => '{645FF040-5081-101B-9F08-00AA002F954E}',
     'videos'    => '{35286a68-3c57-41a1-bbb1-0eae73d76c95}',
